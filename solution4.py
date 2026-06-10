e6552012d60a2bb897acedf5ead080ddb46df326
Manjit Sharma <sharmanjit128a@gmail.com>
2026-06-10 20:43:03 +0530

    Update solution4.py

--- solution4.py
+++ solution4.py
@@ -1,1 +1,20 @@
+Dict1={}
+List1={}
+List2={}
+for i in range(0,10):
+    products=input("enter the product name\n")
+    List1[i]=products
+
+
+    Qty=int(input("Enter the quantity\n"))
+    List2[i]=Qty
+
+
+    Dict1[List1[i]]=List2[i]
+print("\n")
+for i, j in Dict1.items():
+    if(j<10):
+        print(i,"\n")
+
+
 
