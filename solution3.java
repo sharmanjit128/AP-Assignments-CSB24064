c1ae8665829d3f3284aa993030fe3a65fdd8a5f1
Manjit Sharma <sharmanjit128a@gmail.com>
2026-06-10 20:39:04 +0530

    Implement search functionality for names list
    Added functionality to search for a specific name in the list and print its index if found.

--- solution3.java
+++ solution3.java
@@ -1,1 +1,39 @@
+import java.util.*;
+public class A3 {
+    public static void main(String args[]){
+        ArrayList<String> Names=new ArrayList<>();
+        Names.add("Atomic habbits");
+        Names.add("RN KAO");
+        Names.add("R&AW");
+        Names.add("the accidental PM");
+        Names.add("Gandhi");
+        System.out.print(Names);
+        System.out.println();
+        //SEARCHING FOR
+
+
+        String key ="KAO";
+        //System.out.println(Names.contains("Gandhi"));
+        int i=0;
+        for(String element:Names){
+            if(element.contains(key)){
+                System.out.println("Found at index "+ i);
+
+
+            }
+            i++;
+        }
+       
+        return ;
+    }
+
+
+
+
+}
+   
+
+
+
+
 
